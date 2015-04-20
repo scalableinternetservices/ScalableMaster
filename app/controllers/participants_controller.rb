fd0af5ac4908c9a0637529e410cef4ba1014b99d
XingyuGit <xingyuhit@gmail.com>
2015-04-19 23:37:15 -0700

    add participant/organizer's profile show/edit page; add boostrap

---
+++ app/controllers/participants_controller.rb
@@ -0,0 +1,29 @@
+class ParticipantsController < ApplicationController
+  before_action :set_participant, only: [:show, :update]
+
+  def show 
+  end
+
+  def update
+    respond_to do |format|
+      if @participant.update(participant_params)
+        format.html { redirect_to @participant, notice: 'Participant was successfully updated.' }
+        # format.js
+      else
+        format.html { render :show }
+        # format.js
+      end
+    end
+  end
+
+
+  private
+    def set_participant
+      @participant = Participant.find(params[:id])
+    end
+
+    def participant_params
+      params.require(:participant).permit(:name, :nickname, :gender, :age, :phone, :address, :img_url)
+    end
+
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,14 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+10.times do |x| 
+  Organizer.create!(name: "org#{x}", email: "org#{x}@example.com", 
+                    password: "asdfasdf", password_confirmation: "asdfasdf")
+end
+
+
+10.times do |x| 
+  Participant.create!(name: "par#{x}", email: "par#{x}@example.com", 
+                      password: "12341234", password_confirmation: "12341234")
+end
