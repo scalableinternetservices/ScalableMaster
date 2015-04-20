9036a6b11fd6f9a3c18263b5a685adaf5eb2e7a0
Rainee <Rainee@wifi-131-179-38-151.host.ucla.edu>
2015-04-20 16:10:30 -0700

    As a participant, I can click “my activity” in my summary page, in order to see all my activities.

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
+
   devise_for :organizers
   devise_for :participants
   get "/activities/:id" => 'activities#show'
+  get "/participant_activities/:id" => 'participant_activities#show'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
