3c49ee34e974b105cdbc41da17821975129d6dc5
XingyuGit <xingyuhit@gmail.com>
2015-04-24 22:25:24 -0700

    fix name_route for profile

--- app/controllers/participants_controller.rb
+++ app/controllers/participants_controller.rb
@@ -1,11 +1,25 @@
 class ParticipantsController < ApplicationController
 
+	before_action :set_participant, only: [
+    :profile_show, :profile_update, 
+    :cancel_activity, :rejoin_activity,
+    :activities_index
+  ]
 
-	before_action :set_participant, only: [:show, :cancel_activity, :rejoin_activity,:activities_index]
-
-
-	def show
+	def profile_show
 	end
+
+  def profile_update
+    respond_to do |format|
+      if @participant.update(participant_params)
+        format.html { redirect_to @participant, notice: 'Participant was successfully updated.' }
+        # format.js
+      else
+        format.html { render :show }
+        # format.js
+      end
+    end
+  end
 
   def cancel_activity
     @activity = Activity.find(params[:activity_id])
@@ -24,19 +38,6 @@ class ParticipantsController < ApplicationController
     respond_to do |format|
       format.html { redirect_to action: :show }
       format.js
-    end
-  end
-
-
-  def update
-    respond_to do |format|
-      if @participant.update(participant_params)
-        format.html { redirect_to @participant, notice: 'Participant was successfully updated.' }
-        # format.js
-      else
-        format.html { render :show }
-        # format.js
-      end
     end
   end
 
