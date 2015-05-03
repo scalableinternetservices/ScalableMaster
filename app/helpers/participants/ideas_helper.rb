693b70104afae48b8817da827c238f2f4f35a3fc
XingyuGit <xingyuhit@gmail.com>
2015-05-03 00:02:47 -0700

    ideas edit/show with ajax

--- app/helpers/participants/ideas_helper.rb
+++ app/helpers/participants/ideas_helper.rb
@@ -1,2 +1,11 @@
 module Participants::IdeasHelper
+
+  def participant_ideas_or_idea_path(participant, idea)
+    if idea.id
+      participant_idea_path(participant, idea)
+    else 
+      participant_ideas_path(participant)
+    end
+  end
+
 end
