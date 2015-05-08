6d24a64b91bddaf324c9842245439832d79ca283
ysyyork <ysyyork@gmail.com>
2015-05-07 22:31:21 -0700

    one precompile

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -29,8 +29,8 @@ Rails.application.configure do
   # config.assets.css_compressor = :sass
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
-  config.assets.compile = false
-  # config.assets.compile = true
+  # config.assets.compile = false
+  config.assets.compile = true
   # config.assets.precompile =  ['*.js', '*.css', '*.css.erb'] 
 
   # Asset digests allow you to set far-future HTTP expiration dates on all assets,
@@ -79,7 +79,7 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
-  config.serve_static_assets=true
+  config.serve_static_files=true
 end
 
 
