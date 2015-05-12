a235f80f76c923b4351abc2107fd961dcd9c62fe
ysyyork <ysyyork@gmail.com>
2015-05-12 12:18:14 -0700

    add image to activity

--- app/models/activity.rb
+++ app/models/activity.rb
@@ -4,5 +4,6 @@ class Activity < ActiveRecord::Base
 	belongs_to :organizer
 	#attr_accessible :avatar, :avatar_cache, :remove_avatar
 	# mount_uploader :avatar, AvatarUploader
-	has_attached_file :avatar, :styles => { :medium => "300x300>", :thumb => "100x100>" }, :default_url => "/images/:style/missing.png"
+	has_attached_file :avatar, :styles => { :medium => "400x400>", :thumb => "100x100#", :large => "800x800>"}, :default_url => "/images/:style/missing.png"
+    validates_attachment_content_type :avatar, :content_type => ["image/jpg", "image/jpeg", "image/png", "image/gif"]
 end
