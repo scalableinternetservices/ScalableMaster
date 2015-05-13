b435d152d013bd5a69be6337eca57a5be92499f8
ysyyork <ysyyork@gmail.com>
2015-05-13 10:35:58 -0700

    change activity img resolution

--- app/models/activity.rb
+++ app/models/activity.rb
@@ -4,6 +4,6 @@ class Activity < ActiveRecord::Base
 	belongs_to :organizer
 	#attr_accessible :avatar, :avatar_cache, :remove_avatar
 	# mount_uploader :avatar, AvatarUploader
-	has_attached_file :avatar, :styles => { :medium => "400x400>", :thumb => "100x100#", :large => "600x600>"}, :default_url => "/images/:style/missing.png"
+	has_attached_file :avatar, :styles => { :medium => "400x400>", :thumb => "100x100#", :large => "800x500#"}, :default_url => "/images/:style/missing.png"
     validates_attachment_content_type :avatar, :content_type => ["image/jpg", "image/jpeg", "image/png", "image/gif"]
 end
