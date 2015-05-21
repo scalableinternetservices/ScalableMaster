6a2ae8c830e63d4edfbe46c6905555fb5c19fb00
XingyuGit <xingyuhit@gmail.com>
2015-05-21 14:45:43 -0700

    tag match optimization on participant's homepage

--- app/controllers/organizers_controller.rb
+++ app/controllers/organizers_controller.rb
@@ -15,33 +15,8 @@ class OrganizersController < Organizers::BaseController
     else
       @activities = @organizer.activities
     end
-    
-    if participant_signed_in?
-      @participant = Participant.find(current_participant[:id])
-      tags = @participant.tags
-    elsif organizer_signed_in?
-      @organizer = Organizer.find(current_organizer[:id])
-      tags = @organizer.tags
-    else 
-      flag = 1
-    end
-      
-    # @ideas = []
-    # idea_id = []
-    # if flag != 1
-    #   tags.each do |tag|
-    #     idea_tmp = Idea.joins(:tags).where('tags.id = ?', tag)
-
-    #     idea_tmp.each do |idea|
-    #       if !idea_id.include? idea.id
-    #         idea_id << idea.id
-    #         @ideas << idea
-    #       end
-    #     end
-    #   end 
-    # end
-    @ideas = Idea.joins(:tags).where('tags.id IN (?)', tags.select(:id)).distinct
-    
+  
+    @ideas = Idea.joins(:tags).where('tags.id IN (?)', @organizer.tags.select(:id)).distinct
 
   end
 
