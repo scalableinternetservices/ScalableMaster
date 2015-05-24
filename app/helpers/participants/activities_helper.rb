4fa01b295314dba96a201a00895da8dffbcdfedf
ysyyork <ysyyork@gmail.com>
2015-05-24 15:05:55 -0700

    summary fragment cache

--- app/helpers/participants/activities_helper.rb
+++ app/helpers/participants/activities_helper.rb
@@ -1,2 +1,7 @@
 module Participants::ActivitiesHelper
+	def cache_key_for_activities
+		count = Activity.count
+		max_updated_at = Activity.maximum(:updated_at).try(:utc).try(:to_s, :number)
+		"participants-summary-activities-#{count}-#{max_updated_at}"
+	end
 end
--- app/helpers/participants/ideas_helper.rb
+++ app/helpers/participants/ideas_helper.rb
@@ -1,3 +1,7 @@
 module Participants::IdeasHelper
-
+	def cache_key_for_ideas
+		count = Idea.count
+		max_updated_at = Idea.maximum(:updated_at).try(:utc).try(:to_s, :number)
+		"participants-summary-ideas-#{count}-#{max_updated_at}"
+	end
 end
