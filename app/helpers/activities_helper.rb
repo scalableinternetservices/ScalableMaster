4426d8395dc1fcd0e75ea8ce177b11e9769b504b
XingyuGit <xingyuhit@gmail.com>
2015-05-25 17:05:48 -0700

    summary cache

--- app/helpers/activities_helper.rb
+++ app/helpers/activities_helper.rb
@@ -1,2 +1,10 @@
 module ActivitiesHelper
+  def cache_key_for_activities_all(activities)
+    max_updated_at = activities.maximum(:updated_at).try(:utc).try(:to_s, :number)
+    "ideas-all-#{max_updated_at}"
+  end
+
+  def cache_key_for_activities_row(activity)
+    "ideas-row-#{activity.updated_at}"
+  end
 end
