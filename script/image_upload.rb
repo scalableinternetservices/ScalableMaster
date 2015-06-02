886d91faf9f295fb148375a0f1b43a6c7c566f2f
ysyyork <ysyyork@gmail.com>
2015-06-02 12:51:49 -0700

    replication added

--- script/image_upload.rb
+++ script/image_upload.rb
@@ -13,8 +13,8 @@ image_path = '/media/psf/Dropbox/image_upload/'
 # end
 # puts "act done"
 
-i = 1
-while i < 200
+i = 200
+while i < 300
 	img_name = ["1.jpg","10.jpg", "2.jpg", "3.jpg", "4.jpg", "6.jpg", "7.jpg", "IMG_5411.jpg", "IMG_6056.jpg", "IMG_6169.jpg"]
 	act = Activity.find(i)
 	if (!File.directory? "#{image_path}act/#{img_name[(i - 1)%10]}") 
@@ -24,28 +24,28 @@ while i < 200
 end
 puts "act done"
 
-i = 1
-Dir.foreach("#{image_path}org_thumbnail/") do |img|
-	if i > 10
-		break;
-	end
-	act = Organizer.find(i)
-	if (!File.directory? "#{image_path}org_thumbnail/#{img}") && (img != ".DS_Store")
-	    act.update_attribute(:avatar, File.open("#{image_path}org_thumbnail/#{img}", 'r'))
-	    i+=1
-	end
-end
-puts "org done"
+# i = 1
+# Dir.foreach("#{image_path}org_thumbnail/") do |img|
+# 	if i > 10
+# 		break;
+# 	end
+# 	act = Organizer.find(i)
+# 	if (!File.directory? "#{image_path}org_thumbnail/#{img}") && (img != ".DS_Store")
+# 	    act.update_attribute(:avatar, File.open("#{image_path}org_thumbnail/#{img}", 'r'))
+# 	    i+=1
+# 	end
+# end
+# puts "org done"
 
-i = 1
-Dir.foreach("#{image_path}par_thumbnail/") do |img|
-	if i > 10
-		break;
-	end
-	act = Participant.find(i)
-	if (!File.directory? "#{image_path}par_thumbnail/#{img}") && (img != ".DS_Store")
-	    act.update_attribute(:avatar, File.open("#{image_path}par_thumbnail/#{img}", 'r'))
-	    i+=1
-	end
-end
-puts "par done"
+# i = 1
+# Dir.foreach("#{image_path}par_thumbnail/") do |img|
+# 	if i > 10
+# 		break;
+# 	end
+# 	act = Participant.find(i)
+# 	if (!File.directory? "#{image_path}par_thumbnail/#{img}") && (img != ".DS_Store")
+# 	    act.update_attribute(:avatar, File.open("#{image_path}par_thumbnail/#{img}", 'r'))
+# 	    i+=1
+# 	end
+# end
+# puts "par done"
