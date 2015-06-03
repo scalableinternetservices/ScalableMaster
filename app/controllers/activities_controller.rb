af7447bb64be657a70a396a93e7cd5f4dea0ebd1
XingyuGit <xingyuhit@gmail.com>
2015-06-03 14:51:20 -0700

    deep cache

--- app/controllers/activities_controller.rb
+++ app/controllers/activities_controller.rb
@@ -1,35 +1,13 @@
 class ActivitiesController < ApplicationController
-	before_action :get_identity
-	before_action :set_activity, only: [:show]
-	
-
+  
 	def show
+    if participant_signed_in?
+      redirect_to participant_activity_path(current_participant[:id], params[:id])
+    elsif organizer_signed_in?
+      redirect_to organizer_activity_path(current_organizer[:id], params[:id])
+    else
+      redirect_to new_participant_registration_path
+    end
 	end
-
-	private
-	  def set_activity
-	  	if @identity == 0
-	  		redirect_to new_participant_registration_path
-	  	elsif @identity == 1
-	  		@activity = Activity.find(current_participant[:id])
-	  	else
-	  		@activity = Activity.find(current_organizer[:id])
-	  	end
-    end
-
-    # def activity_params
-    #   params.require(:activity).permit(:name, :time, :creator_id, :place_id, :address, :avg_price, :visibility, :pay_type, :scale, :latitude, :longitude)
-    # end
-    def get_identity
-    	# byebug
-    	if participant_signed_in?
-    		@identity = 1
-    	elsif organizer_signed_in?
-    		@identity = 2
-    	else
-    		@identity = 0
-    	end
-    end
-    			
 
 end
