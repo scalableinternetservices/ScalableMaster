d65d9e9578c8a588db2027dac0146c3b36d558ba
XingyuGit <xingyuhit@gmail.com>
2015-06-04 18:27:38 -0700

    optimize cache_key

--- app/helpers/activities_helper.rb
+++ app/helpers/activities_helper.rb
@@ -1,12 +1,12 @@
 module ActivitiesHelper
   def cache_key_for_activities_all(activities, page)
-    # count = activities.count
-    # max_updated_at = activities.maximum(:updated_at).try(:utc).try(:to_s, :number)
-    # "activities-#{page}-all-#{count}-#{max_updated_at}"
+    count = activities.count
+    max_updated_at = activities.maximum(:updated_at).try(:utc).try(:to_s, :number)
+    "activities-#{page}-all-#{count}-#{max_updated_at}"
 
-    ids = activities.map(&:id).join('-')
-    max_updated_at = activities.map(&:updated_at).max
-    "activities-#{page}-all-#{max_updated_at}-#{ids}"
+    # ids = activities.map(&:id).join('-')
+    # max_updated_at = activities.map(&:updated_at).max
+    # "activities-#{page}-all-#{max_updated_at}-#{ids}"
   end
 
   def cache_key_for_activities_row(activity, page)
